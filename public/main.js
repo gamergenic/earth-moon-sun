32dbbc0de7bb7ef0ca982e3ba928857cf69c0663
gamergenic <82328213+gamergenic@users.noreply.github.com>
2023-12-27 11:19:48 -0800

    Mostly right!

--- public/main.js
+++ public/main.js
@@ -1,7 +1,7 @@
 // const THREE = require('three'); // Not required since Three.js is included via CDN
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
 const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('myCanvas') });
 
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -14,7 +14,7 @@ const moonTexture = textureLoader.load('8k_moon.jpg');
 const sunTexture = textureLoader.load('8k_sun.jpg');
 
 // Geometry for a sphere
-const earthGeometry = new THREE.SphereGeometry(1, 32, 32);
+const earthGeometry = new THREE.SphereGeometry(1, 128, 128);
 
 // Lit material with texture
 const earthMaterial = new THREE.MeshStandardMaterial({ map: earthTexture });
@@ -23,23 +23,23 @@ const earthMaterial = new THREE.MeshStandardMaterial({ map: earthTexture });
 const earthSphere = new THREE.Mesh(earthGeometry, earthMaterial);
 scene.add(earthSphere);
 
-const moonGeometry = new THREE.SphereGeometry(1, 32, 32);
+const moonGeometry = new THREE.SphereGeometry(1, 64, 64);
 const moonMaterial = new THREE.MeshStandardMaterial({ map: moonTexture });
 const moonSphere = new THREE.Mesh(moonGeometry, moonMaterial);
 scene.add(moonSphere);
-moonSphere.position.set(-5, 3, 5);
+//moonSphere.position.set(-5, 3, 5);
 
 
-const sunGeometry = new THREE.SphereGeometry(1, 32, 32);
+const sunGeometry = new THREE.SphereGeometry(1, 64, 64);
 const sunMaterial = new THREE.MeshBasicMaterial({ map: sunTexture });
 const sunSphere = new THREE.Mesh(sunGeometry, sunMaterial);
 scene.add(sunSphere);
-sunSphere.position.set(5, 3, 5);
+//sunSphere.position.set(500, 300, 500);
 
 
 // Directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1); // Color and intensity
-directionalLight.position.set(5, 3, 5); // Position of the light
+directionalLight.position.set(500, 300, 500); // Position of the light
 scene.add(directionalLight);
 
 camera.position.z = 5;
@@ -51,8 +51,22 @@ controls.target = earthSphere.position;
 const socket = io();
 
 socket.on('earthRotation', (rotationY) => {
-    earthSphere.rotation.y = rotationY;
+//    earthSphere.rotation.y = rotationY;
 });
+
+socket.on('earthPosition', (position) => {
+    earthSphere.position.set(position.x, position.y, position.z);
+});
+
+socket.on('moonPosition', (position) => {
+    moonSphere.position.set(position.x, position.y, position.z);
+});
+
+socket.on('sunPosition', (position) => {
+    sunSphere.position.set(position.x, position.y, position.z);
+    directionalLight.position.set(position.x, position.y, position.z);
+});
+
 
 socket.on('moonScale', (scale) => {
     moonSphere.scale.x = scale;
@@ -70,6 +84,19 @@ socket.on('sunScale', (scale) => {
     sunSphere.scale.x = scale;
     sunSphere.scale.y = scale;
     sunSphere.scale.z = scale;
+});
+
+
+socket.on('earthQuat', (quat) => {
+    earthSphere.quaternion.set(quat.x, quat.y, quat.z, quat.w);
+});
+
+socket.on('moonQuat', (quat) => {
+    moonSphere.quaternion.set(quat.x, quat.y, quat.z, quat.w);
+});
+
+socket.on('sunQuat', (quat) => {
+    sunSphere.quaternion.set(quat.x, quat.y, quat.z, quat.w);
 });
 
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const {spice, genericKernels, et_now} = require('./modules/js-spice');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,18 +19,63 @@ let rotationY = 0; // Initialize rotationY
 
 io.on('connection', (socket) => {
 //    console.log('A user connected');
-    socket.emit('moonScale', moonScale);
-    socket.emit('earthScale', earthScale);
-    socket.emit('sunScale', sunScale);
+    let moonScale = spice.bodvrd('moon', 'RADII');
+    let earthScale = spice.bodvrd('earth', 'RADII');
+    let sunScale = spice.bodvrd('sun', 'RADII');
+    let scaleDivisor = earthScale[0];
+    socket.emit('moonScale', moonScale[0]/scaleDivisor);
+    socket.emit('earthScale', earthScale[0]/scaleDivisor);
+    socket.emit('sunScale', sunScale[0]/scaleDivisor);
+
+    const ref = 'ECLIPJ2000';
+    const abcorr = 'NONE';
+    const obs = 'EARTH_BARYCENTER';
+    const sceneRotation = spice.rotate(spice.halfpi(), 1);
+    const spherePreRotation = spice.eul2m(-spice.halfpi(), 0, 0, 1, 2, 3);
+
+    var moonPos;
+    var earthPos;
+    var sunPos;
+    var et = et_now();
 
     // Emit sphere rotation value every second
-    setInterval(() => {
-        rotationY += 0.001; // Update rotationY
-        socket.emit('earthRotation', rotationY);
-    }, 1000);
+    const intervalId = setInterval(() => {
+    
+        // et = et_now();
+        et += spice.spd()/1200;
+
+        // positions
+        sunPos = spice.spkpos('sun', et, ref, abcorr, obs).ptarg;
+        earthPos = spice.spkpos('earth', et, ref, abcorr, obs).ptarg;
+        moonPos = spice.spkpos('moon', et, ref, abcorr, obs).ptarg;
+
+        sunPos = spice.mxv(sceneRotation, sunPos);
+        earthPos = spice.mxv(sceneRotation, earthPos);
+        moonPos = spice.mxv(sceneRotation, moonPos);
+
+        const sunPosition = {'x': sunPos[0] / scaleDivisor, 'y': sunPos[1] / scaleDivisor, 'z' : sunPos[2] / scaleDivisor };
+        const earthPosition = {'x': earthPos[0] / scaleDivisor, 'y': earthPos[1] / scaleDivisor, 'z' : earthPos[2] / scaleDivisor };
+        const moonPosition = {'x': moonPos[0] / scaleDivisor, 'y': moonPos[1] / scaleDivisor, 'z' : moonPos[2] / scaleDivisor };
+
+        socket.emit('sunPosition', sunPosition);
+        socket.emit('earthPosition', earthPosition);
+        socket.emit('moonPosition', moonPosition);
+
+        // rotations
+        // Get the rotation matrix from the origin's frame to the Body frame.
+        var earthLocalRotation = spice.pxform('IAU_EARTH', ref, et);
+        var earthTotalRotation = spice.mxm(earthLocalRotation, spherePreRotation);
+        earthTotalRotation = spice.mxm(sceneRotation, earthTotalRotation);
+      
+        socket.emit('earthQuat', spice.m2q(earthTotalRotation));
+        socket.emit('moonQuat', spice.m2q(spice.mxm(sceneRotation, spice.pxform('IAU_MOON', ref, et))));
+        socket.emit('sunQuat', spice.m2q(spice.mxm(sceneRotation, spice.pxform('IAU_SUN', ref, et))));
+
+    }, 20);
 
     socket.on('disconnect', () => {
 //        console.log('A user disconnected');
+        clearInterval(intervalId);
     });
 });
 
@@ -37,3 +83,33 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+
+
+async function getKernels() {
+
+    const kernelsToLoad = [
+        'lsk/latest_leapseconds.tls',
+        'pck/pck00011.tpc',
+        'pck/gm_de440.tpc',
+        'spk/planets/de440.bsp',
+        'spk/stations/earthstns_itrf93_201023.bsp',
+        'fk/stations/earth_topo_201023.tf',
+        'pck/earth_200101_990825_predict.bpc'
+    ];
+
+
+    async function loadAndProcessFiles(files) {
+        const operations = files.map(file => {
+            return genericKernels.getGenericKernel(file, `data/naif/generic_kernels/${file}`).then(kernel => {
+                spice.furnsh(kernel);
+            });
+        });
+    
+        await Promise.all(operations);
+        console.log("done loading kernels");
+    }
+    await loadAndProcessFiles(kernelsToLoad);
+}
+
+getKernels();
